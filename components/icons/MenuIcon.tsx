4b75cafb848f0e3ff85e8e7e5e86172bc113a62a
SHipify-World <sanjaybsarode@gmail.com>
2025-09-06 23:31:26 +0530

    Fix: Recreate MenuIcon.tsx to resolve Vercel build issue

---
+++ components/icons/MenuIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MenuIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M4 6h16M4 12h16m-7 6h7"
+    />
+  </svg>
+);
+
+export default MenuIcon;
